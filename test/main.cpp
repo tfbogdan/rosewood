5caa44a6186e000ee53392175383176ffda0a101
bogdan tudoran <tfbogdan@gmail.com>
2018-11-12 19:17:56 +0100

    use google test for testing

--- test/main.cpp
+++ test/main.cpp
@@ -2,12 +2,15 @@
 #include "Jinx.metadata.h"
 
 #include <mc/mc.hpp>
-#include <fmt/printf.h>
-#include <iostream>
 #include <functional>
 #include <memory>
+#include <string>
+#include <vector>
 
-int main() {
+#include <gtest/gtest.h>
+
+
+TEST(MC, compiletime) {
     using Jinx = mc::meta<jinx::Jinx>;
     using JinxTypes = mc::meta<jinx::JinxTypes>;
 
@@ -19,18 +22,23 @@ int main() {
     static_assert (!jinxTypes.in_range(-27));
     static_assert (jinxTypes.in_range(-32));
 
-    // auto aMethod = jinx.find_method<Jinx::methods>("aMethod");
-    // using aMethodType = decltype (aMethod);
+    std::vector<std::pair<std::string, int>> jinxEnumerators;
 
-    // static_assert (!std::is_same<aMethodType, mc::nil_t>::value);
-
-    jinxTypes.for_each_enumerator([](const auto &en){
-        fmt::print("  {}: {:d}\n", en.name, en.value);
+    jinxTypes.for_each_enumerator([&jinxEnumerators](const auto &en){
+        jinxEnumerators.emplace_back(en.name, en.value);
     });
 
+    const std::vector<std::pair<std::string, int>> expectedEnums {
+        std::pair("underJinx", -32),
+        std::pair("overUnderJinx", -31),
+        std::pair("zeroJinx", 0),
+        std::pair("moreJinx", 1),
+        std::pair("superJinx", 100)
+    };
+
+    EXPECT_EQ (jinxEnumerators, expectedEnums);
 
     std::unique_ptr<mc::DynamicClass> dynamicJinx(new mc::DynamicClassWrapper<jinx::Jinx>);
-    fmt::print("Jinx has a `{}` method: {}\n", "no such method", dynamicJinx->hasMethod("no such method"));
-    fmt::print("Jinx has a `{}` method: {}\n", "aMethod", dynamicJinx->hasMethod("aMethod"));
-    return 0;
+    EXPECT_FALSE(dynamicJinx->hasMethod("no such method"));
+    EXPECT_TRUE(dynamicJinx->hasMethod("aMethod"));
 }
